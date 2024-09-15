62df0316bc0d29a412d8494775c80d3a5af583be
wilopez8 <53378536+wilopez8@users.noreply.github.com>
2024-09-15 01:14:55 -0400

    Update 1_Clientes.py

--- pages/1_Clientes.py
+++ pages/1_Clientes.py
@@ -83,7 +83,7 @@ with st.form("formulario_ingreso_clientes", clear_on_submit=True):
         client = influxdb_client.InfluxDBClient(url=url,token=token,org=org)
         write_api = client.write_api(write_options=SYNCHRONOUS)
         p = (
-        influxdb_client.Point("Cliente2")
+        influxdb_client.Point("Cliente3")
         .tag("Apellido")
         .field("Estado", estado_cliente)
         )
